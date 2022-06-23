f7e666bb0dfbc8fd7ac2d81614a89ea5376bac92
MMTabaka <miloszmtabaka@gmail.com>
2022-06-23 01:29:10 +0100

    View composition

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,13 +7,33 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
 struct ContentView: View {
     @State private var useRedText = false
     
     var body: some View {
-        Text("Hello")
-            .padding()
-        Text("World")
+        VStack {
+            CapsuleText(text: "First")
+            
+            Text("Second")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(.blue)
+                .clipShape(Capsule())
+        }
     }
 }
 
